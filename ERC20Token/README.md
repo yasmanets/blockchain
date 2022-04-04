7317230b0d9572989df339a647b27f6006a14a86
yaser-eda-teamswipe <yaser.94ae@gmail.com>
2022-04-04 18:32:44 +0200

    erc 20

---
+++ ERC20Token/README.md
@@ -0,0 +1,5 @@
+# YAS Token - ERC20
+
+YAS Token - ERC20 token implementation example
+
+[Mumbai Scan](https://mumbai.polygonscan.com/token/0x83498f7b75ac75ec3e970e99a56c8d3d8de05b75#balances)
---
+++ ERC20Token/SafeMath.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 >=0.8.0;
+pragma experimental ABIEncoderV2;
+
+// Source: "https://gist.github.com/giladHaimov/8e81dbde10c9aeff69a1d683ed6870be"
+
+library SafeMath {
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+    return c;
+  }
+}
